881e30dff633de6bc85fae7615eba2cf6d9d5476
m242 <m242@pm.me>
2021-09-13 20:22:59 +0200

    /state and / done, base of /local_vote

--- qfd/main.py
+++ qfd/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-from flask import Flask
-
+from flask import Flask, redirect, url_for, request, make_response, render_template
+import uuid
+import datetime
+from helpers import generate_player_name, check_cookie_validity
 
 ## Setting up a proper Flask logging
 from logging.config import dictConfig
@@ -32,14 +34,63 @@ app = Flask(__name__)
 def index():
     """This is the main page. It checks if a user already has a name, gives them one
     if needed, and redirects to either local_vote or distant_vote"""
-    app.logger.info("got a hit on index")
-    return ""
+    id_cookie = request.cookies.get("id")
+    # The user exists
+    # Gotta play everyday to keep the cookie
+    if check_cookie_validity(id_cookie, app.config["STATE"]):
+        app.config["STATE"]["players"]["last_active"] = datetime.now()
+        if app.config["STATE"]["players"]["is_local"]:
+            app.logger.debug(f"local player {uuid.UUID(id_cookie)} joined back")
+            return redirect(url_for("local_vote"))
+        else:
+            app.logger.debug(f"distant player {id_cookie} joined back")
+            return redirect(url_for("distant_vote"))
+
+    # The user is entirely new/has an expired cookie
+    player_id = str(uuid.uuid4())
+    player = {
+        "name": generate_player_name(),
+        "score": 0,
+        "last_active": datetime.datetime.now(),
+        "is_local": True,
+    }
+    app.config["STATE"]["players"][player_id] = player
+
+    app.logger.info("Created player {}, named {}".format(player_id, player["name"]))
+
+    resp = make_response(redirect(url_for("local_vote")))
+    resp.set_cookie("id", str(player_id))
+
+    return resp
 
 
-@app.route("/local_connect")
-def local_connect():
-    player = request.cookies.get("id")
-    return ""
+@app.route("/local_vote")
+def local_vote():
+    if request.cookies.get("id") is None:
+        app.logger.info(
+            f"{request.remote_addr} hit /local_vote without having a cookie"
+        )
+        # TODO this is probably worth monitoring
+        return "Please auth", 403
+
+    return render_template("local_vote.html", state=app.config["STATE"])
+
+
+@app.route("/state")
+def state():
+    """This function sends back the current state, it's polled by frontend
+    to update the pages."""
+    if request.cookies.get("id") is None:
+        app.logger.info(
+            f"{request.remote_addr} hit /local_vote without having a cookie"
+        )
+        # TODO this is probably worth monitoring
+        return "Please auth", 403
+
+    return app.config["STATE"]
+
+
+# TODO doc of STATE
 
 
 if __name__ == "__main__":
@@ -47,6 +98,6 @@ if __name__ == "__main__":
 
     # TODO using config to hold state isn't great, but I don't have a better
     # idea
-    app.config["STATE"] = {"question": {}, "players": []}
+    app.config["STATE"] = {"question": {}, "players": {}}
 
     app.run(debug=True, host="0.0.0.0")
